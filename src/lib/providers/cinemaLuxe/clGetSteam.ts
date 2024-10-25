8e1f863aa889dc81c09c667844eff3ba7c6e3162
punisher-303 <mehafin0@gmail.com>
2024-10-26 03:42:53 +0530

    added autoembededanime , autoembededDrama & cinemaluxe

---
+++ src/lib/providers/cinemaLuxe/clGetSteam.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import {Stream} from '../types';
+import {hubcloudExtracter} from '../hubcloudExtractor';
+import {gdFlixExtracter} from '../gdflixExtractor';
+
+export const clGetStream = async (
+  link: string,
+  type: string,
+  signal: AbortSignal,
+): Promise<Stream[]> => {
+  try {
+    let newLink = link;
+    if (!link.includes('hubcloud') && !link.includes('gdflix')) {
+      console.log('link', link);
+      const res = await axios.get(link);
+      const data = res.data;
+      const encodedLink = data.match(/"link":"([^"]+)"/)[1];
+      newLink = encodedLink ? atob(encodedLink) : link;
+    }
+    console.log('newLink', newLink);
+    if (newLink.includes('gdflix')) {
+      const sreams = await gdFlixExtracter(newLink, signal);
+      return sreams;
+    }
+    const res2 = await axios.get(newLink, {signal});
+    const data2 = res2.data;
+    const hcLink = data2.match(/location\.replace\('([^']+)'/)?.[1] || newLink;
+    const hubCloudLinks = await hubcloudExtracter(
+      hcLink.includes('https://hubcloud') ? hcLink : newLink,
+      signal,
+    );
+    return hubCloudLinks;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
