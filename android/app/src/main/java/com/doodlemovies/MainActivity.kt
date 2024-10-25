481aa303c69e61fa94f62a261a7aadfbf07cfc3c
punisher-303 <mehafin0@gmail.com>
2024-10-26 03:02:31 +0530

    New Doodle App Pushing

---
+++ android/app/src/main/java/com/doodlemovies/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.doodle
+import expo.modules.ReactActivityDelegateWrapper
+
+import android.os.Bundle
+import com.zoontek.rnbootsplash.RNBootSplash
+import androidx.core.view.WindowCompat
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "Doodle"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled))
+  
+   override fun onCreate(savedInstanceState: Bundle?) {
+    RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
+    super.onCreate(savedInstanceState)
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+  }
+}
